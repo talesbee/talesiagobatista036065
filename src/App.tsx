355213b2ffd15bb67a4f1a3db6cd7059752ea300
talesbee <talesiago@gmail.com>
2026-01-26 17:56:30 -0400

    feat: ajuste no seletor de idiomas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
-
-import AppRoutes from './routes/AppRoutes';
-import { Header } from './components';
-
+import AppRoutes from "./routes/AppRoutes";
+import { Header } from "./components";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-100 via-white to-blue-200">
       <Header />
-      <div className="flex-1 pt-20">
+      <div className="flex-1 pt-5">
         <AppRoutes />
       </div>
     </div>
