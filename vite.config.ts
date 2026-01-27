d00c4a2e12488b9ec3e50cbc857915c99b6798dd
talesbee <talesiago@gmail.com>
2026-01-27 17:19:24 -0400

    fix(svg): corrigir importação de SVG como componente React

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [svgr(), react()],
   server: {
     open: true,
   },
