edc9aab97b26920d8a2857c9518d4ead4677ae2f
talesbee <talesiago@gmail.com>
2026-02-01 16:34:33 -0400

    feat(icons): adicionar novos ícones ao projeto

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,4 +1,9 @@
 import Close from './Close.svg?react';
 import Upload from './Upload.svg?react';
+import Edit from './Edit.svg?react';
+import Error from './Error.svg?react';
+import Success from './Success.svg?react';
+import Loading from './Loading.svg?react';
+import Warning from './Warning.svg?react';
 
-export { Close, Upload };
+export { Close, Upload, Edit, Error, Success, Loading, Warning };
