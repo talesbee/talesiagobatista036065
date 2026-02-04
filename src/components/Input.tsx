a39e43ce674275e87aa859cbdde9fccae9e0ac1e
talesbee <talesiago@gmail.com>
2026-02-03 21:41:05 -0400

    fix(i18n): corrigir e adicionar chaves de tradução, substituir literais

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,7 +14,6 @@ const Input: React.FC<InputProps> = ({
   inputClassName = 'input input-bordered px-3 py-2 ml-2 rounded border border-gray-300 focus:ring-2 focus:ring-blue-300',
   ...props
 }) => {
-  console.log('Rendering Input:', { label, props });
   return (
     <label className={containerClassName}>
       <span className={labelClassName}>{label}</span>
